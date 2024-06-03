34351b5475365d3e432057a43b929e932bdd659a
agata <agataomasta05@gmail.com>
2024-06-03 22:31:04 +0200

    init

---
+++ packages/web/src/index.tsx
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.getElementById('app');
+    if (app) {
+      app.innerHTML = '<p>Hello from TypeScript!</p>';
+    }
+  });
