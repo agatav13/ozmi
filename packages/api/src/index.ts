be0fe20eb9f1b3f8a1e1dff100fb286bfdf5adb6
agata <agataomasta05@gmail.com>
2024-06-09 23:35:25 +0200

    post endpoint

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,20 +1,29 @@
-import cors from 'cors'
-import express from 'express'
+import cors from 'cors';
+import express from 'express';
 
-import { Workspace } from 'types'
+import { Workspace } from 'types';
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 3000;
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: `http://localhost:${port}` }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/workspaces', (_, response) => {
   const workspaces: Workspace[] = [
     { name: 'api', version: '1.0.0' },
     { name: 'types', version: '1.0.0' },
     { name: 'web', version: '1.0.0' },
-  ]
+  ];
   response.json({ data: workspaces })
-})
+});
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
+app.post('/data', (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content
+  res.send(`Tytuł: ${title} Treść: ${content}`)
+});
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
