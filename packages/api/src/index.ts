d408731b14a13d3ff8aac5664f693d88eb9de0b5
agata <agataomasta05@gmail.com>
2024-06-14 18:52:18 +0200

    port change

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import { Workspace } from 'types';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(cors({ origin: `http://localhost:${port}` }));
 
