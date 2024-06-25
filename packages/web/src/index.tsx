3b3d7c5e6c52a7cac6af0a8ce178ff6c3f9458c6
agata <agataomasta05@gmail.com>
2024-06-25 15:12:58 +0200

    structure change

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,25 +1,14 @@
-import React, { StrictMode, useEffect, useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { Workspace } from 'types';
+import './index.css'
+import App from './App';
 
-function App() {
-  const [data, setData] = useState<Workspace[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/workspaces')
-      .then((response) => response.json())
-      .then(({ data }) => setData(data));
-  }, []);
-
-  return (
-    <StrictMode>
-      <h1>Building a fullstack TypeScript project with Turborepo</h1>
-      <h2>Workspaces</h2>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </StrictMode>
+const app = document.getElementById('app');
+if (app) {
+  ReactDOM.createRoot(app).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   );
 }
-
-const app = document.querySelector('#app');
-if (app) createRoot(app).render(<App />);
