64e6fc03731365b9ee8d9e905b0d4dd5034babe6
agata <agataomasta05@gmail.com>
2024-07-02 15:11:28 +0200

    theme comments

--- packages/web/src/adminPanel/assets/themes/theme.ts
+++ packages/web/src/adminPanel/assets/themes/theme.ts
@@ -1,27 +1,28 @@
 export const theme1 = {
     token: {
-        colorPrimary: '#7091E6',
+        colorPrimary: '#7091E6',  // główny kolor
         colorInfo: '#7091E6',
-        colorLink: '#7091E6'
+        colorLink: '#7091E6',
+        colorBgLayout: '#f5f5f5'  // kolor tła
     },
     components: {
         Layout: {
-            siderBg: "#2a386e",
-            headerBg: "#2a386e"
+            siderBg: "#2a386e",  // kolor SideBar
+            headerBg: "#2a386e"  // kolor Header
         },
         Menu: {
-            itemBg: '#2a386e',
-            subMenuItemBg: '#242f5c',
-            itemHoverBg: '#4c639c',
-            itemSelectedBg: '#7091E6',
-            itemActiveBg: '#4c639c',
+            itemBg: '#2a386e',  // kolor tła menu
+            subMenuItemBg: '#242f5c',  // kolor tła w rozwiniętym menu
+            itemHoverBg: '#4c639c',  // kolor elementu menu po najechaniu
+            itemSelectedBg: '#7091E6',  // kolor elementu menu kiedy jest wybrany
+            itemActiveBg: '#4c639c',  // kolor elementu menu w momencie kliknięcia
             groupTitleColor: '#EDE8F5',
-            itemColor: '#EDE8F5',
+            itemColor: '#EDE8F5',  // kolor napisów na elementach menu
             itemHoverColor: '#EDE8F5',
             itemSelectedColor: '#EDE8F5'
         },
         Typography: {
-            colorTextHeading: '#f5f5f5'
+            colorTextHeading: '#f5f5f5'  // kolor nagłówka
         }
     }
 }
