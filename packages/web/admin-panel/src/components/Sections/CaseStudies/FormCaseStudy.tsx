08bcd3c8cdf9cd0a53a6e92a4f9b3038c41b9c2b
agatav13 <agataomasta05@gmail.com>
2024-09-22 13:34:05 +0200

    add buttons for text and photos in case studies

--- packages/web/admin-panel/src/components/Sections/CaseStudies/FormCaseStudy.tsx
+++ packages/web/admin-panel/src/components/Sections/CaseStudies/FormCaseStudy.tsx
@@ -2,6 +2,8 @@ import { CaseStudyDataType, CaseStudyDataTypeWithId } from "types";
 import DateInput from "../../reusable/DateInput";
 import { useState } from "react";
 import SelectInput from "../../reusable/SelectInput";
+import { IoTextOutline } from "react-icons/io5";
+import { HiOutlinePhoto } from "react-icons/hi2";
 
 interface FormCaseStudyProps {
   updateShowForm: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,8 +11,6 @@ interface FormCaseStudyProps {
 }
 
 export default function FormCaseStudy({ updateShowForm, onPostAdded }: FormCaseStudyProps) {
-  type HTMLElementEvent = React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>
-
   const caseStudyData: CaseStudyDataType = {
     title: "",
     date: new Date(),
@@ -19,22 +19,25 @@ export default function FormCaseStudy({ updateShowForm, onPostAdded }: FormCaseS
 
   const [responseBody, setResponseBody] = useState<CaseStudyDataType>(caseStudyData);
 
-  const handleChange = (event: HTMLElementEvent) => {
-    const {name, value} = event.target
-    setResponseBody({...responseBody, [name]: value})
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setResponseBody({ ...responseBody, [name]: value });
   };
 
-  const handleSubmit =  async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     console.log("Submitting form data:", responseBody);
 
     try {
-      const response = await fetch("http://localhost:5000/create-case-study-posts", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(responseBody)
-      });
+      const response = await fetch(
+        "http://localhost:5000/create-case-study-posts",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(responseBody),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error status: ${response.status}`);
@@ -42,30 +45,39 @@ export default function FormCaseStudy({ updateShowForm, onPostAdded }: FormCaseS
 
       const result = await response.json();
       onPostAdded({ ...responseBody, id: result.id }); // żeby po dodaniu nowego postu wyświetlał się bez odświeżania
-      setResponseBody(caseStudyData);  // ustawia wartości, które były w poście jako reponseBody
-      updateShowForm(false);  // ukrywa element dodawania postu po dodaniu
+      setResponseBody(caseStudyData); // ustawia wartości, które były w poście jako reponseBody
+      updateShowForm(false); // ukrywa element dodawania postu po dodaniu
     } catch (error) {
       console.error("Error:", error);
     }
-  }
+  };
 
-  return(
+  return (
     <form className="Form" onSubmit={handleSubmit}>
       <label htmlFor="title">Tytuł</label>
       <input type="text" name="title" id="title" required onChange={(e)=>handleChange(e)} />
 
       <label htmlFor="date">Data</label>
-      <DateInput name="date" id="date" onChange={(date) => setResponseBody({...responseBody, date})} />
-
-      <label htmlFor="category">Kategoria</label>
-      <SelectInput 
-        name="category" 
-        id="category" 
-        options={["Przemysł", "E-commerce", "Inne"]} 
-        onChange={handleChange} 
+      <DateInput
+        name="date"
+        id="date"
+        onChange={(date) => setResponseBody({ ...responseBody, date })}
       />
 
-      <input className="AddNewButton" type="submit" value="Dodaj" style={{marginTop: "16px"}} />
+      <label htmlFor="category">Kategoria</label>
+      <SelectInput
+        name="category"
+        id="category"
+        options={["Przemysł", "E-commerce", "Inne"]}
+        onChange={handleChange}
+      />
+
+      <div className="CaseStudiesButtons">
+        <button><IoTextOutline /> Dodaj tekst</button>
+        <button><HiOutlinePhoto />Dodaj zdjęcie</button>
+      </div>
+
+      <input className="AddNewButton" type="submit" value="Dodaj" />
     </form>
   );
 }
